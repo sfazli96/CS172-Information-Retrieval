4ef3527314b6ccb17c441c317d03d25877f6a9e7
sfazli96 <sfazli96@gmail.com>
2021-04-23 16:03:23 -0700

    stemming works

--- parsing.py
+++ parsing.py
@@ -1,7 +1,26 @@
 import re
 import os
 import zipfile
+# import string library function 
+import string 
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
+class term_attributes:
+  
+  def __init__(self, doc_id , list_of_pos, frequency):
+    self.doc_id = doc_id
+    self.list_of_pos = list_of_pos
+    self.frequency = frequency
+
+  def get_id(self):
+      return self.doc_id
+ 
+  def get_list_of_pos(self):
+      return self.list_of_pos
+  
+  def get_frequency(self):
+      return self.frequency
 
 # Regular expressions to extract data from the corpus
 doc_regex = re.compile("<DOC>.*?</DOC>", re.DOTALL)
@@ -9,11 +28,18 @@ docno_regex = re.compile("<DOCNO>.*?</DOCNO>")
 text_regex = re.compile("<TEXT>.*?</TEXT>", re.DOTALL)
 
 # define punctuation
-punctuations = '''!()-[]{};:'"\,\`<>./?@#$%^&*_~'''
 stopwords_set = set()
 
 #where we'll store all our words from the text
 word_dic = {}
+#where we'll store all our docno's and unique terms, total terms , etc.
+docno_dic = {}
+
+# inverted indices to map the ids
+# tokenization process is as a conversion from a document to a sequence of (term_id, doc_id, position) 
+# tuples which need to be stored in your inverted index.
+termIndex = {}
+docIndex = {}
 
 with zipfile.ZipFile("ap89_collection_small.zip", 'r') as zip_ref:
     zip_ref.extractall()
@@ -29,52 +55,89 @@ with open('stopwords.txt') as f:
 for dir_path, dir_names, file_names in os.walk("ap89_collection_small"):
     allfiles = [os.path.join(dir_path, filename).replace("\\", "/") for filename in file_names if (filename != "readme" and filename != ".DS_Store")]
     
+x = 1
+print("Length of all files = "+str(len(allfiles)))
+
 for file in allfiles:
+
+    print("in file number = "+str(x))
     with open(file, 'r', encoding='ISO-8859-1') as f:
         filedata = f.read()
         result = re.findall(doc_regex, filedata)  # Match the <DOC> tags and fetch documents
 
-        x = 1
-        #for every document-- get the doc# and the doc#'s text
+         #for every document-- get the doc# and the doc#'s text
+        print("There are  "+str(len(result))+" documents in this document")
         for document in result[0:1]:
+            
             # Retrieve contents of DOCNO tag
             docno = re.findall(docno_regex, document)[0].replace("<DOCNO>", "").replace("</DOCNO>", "").strip()
+            
             # Retrieve contents of TEXT tag
             text = "".join(re.findall(text_regex, document))\
                       .replace("<TEXT>", "").replace("</TEXT>", "")\
                       .replace("\n", " ")
-            
-            # step 1 - lower-case words, remove punctuation, remove stop-words, etc. 
-            text = text.lower() # lower-case words
 
-            word = ""
-            for char in text:
-                if char in punctuations: # remove punctuation
-                    text = text.replace(char,'')
-                elif char.isspace() == False:
-                    word+=char
-                elif char.isspace() == True:
-                    if word in stopwords_set:
-                        text=text.replace(' ' + word + ' ', ' ')  # Removes stopwords
-                        word=""
-                    #input into our token dictionary
-                    if word not in word_dic:
-                        word_dic[word] = 1
-                        word=""
-                    else:
-                        word_dic[word]+=1
-                        word=""
- 
-            print("Doc#: "+docno)
+            #remove punctuation
+            text = text.translate(str.maketrans('', '', string.punctuation))
+            
+            text = text.lower() 
+            words = text.split() 
+
+            local_dic = {}
+            
+            ps = PorterStemmer()
+            for w in words:
+                print(w, " : ", ps.stem(w))
+                
+            # but first lets define how many times a word occurs and at what position
+            for i, word in enumerate(words,start=1):
+                if word in local_dic:
+                    frequency = local_dic[word][1] + 1
+                    local_dic[word][0].append(i)
+                    local_dic[word][1] = frequency
+
+                else:
+                    local_dic[word] = [[i],1]
+
+
+            total_terms = 0
+            
+            for term in local_dic:
+                # print(term, '->', local_dic[term])
+                mytuple = (docno,local_dic[term])
+                frequency_of_term = len(local_dic[term][0])
+                total_terms = total_terms + frequency_of_term
+                # print(mytuple)
+
+                if term not in word_dic:
+                    if term not in stopwords_set:
+                        word_dic[term] = [mytuple]
+                else:
+                    word_dic[term].append(mytuple)
+            
+                
             print("Text: "+text)
+            docno_dic[docno] = (total_terms,len(local_dic))
+            print("Doc#: "+docno+" total terms: "+str(total_terms)+" unique terms: "+ str(len(local_dic))) 
+            print("Length of word dic at the end = "+str(len(word_dic)))
 
-            if 'has' in word_dic:
-                print("value of has = "+ str(word_dic['has']))
-            
-            
-            
+            for t in word_dic:
+                print(t,'->',word_dic[t])
 
+
+            exit(0) #testing on one document for now
+            #we can remove the exit after we are positive our implementation works
+                    
+
+            
+    x = x +1 #increment what file we are on
+    print("size of dic = "+str(len(word_dic)))
+
+
+print(sorted(word_dic))
+
+
+            
             # step 2 - create tokens 
-            
             # step 3 - build index
             
